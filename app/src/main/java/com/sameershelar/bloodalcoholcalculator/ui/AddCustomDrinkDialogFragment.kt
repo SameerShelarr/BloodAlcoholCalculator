05f2a9e6e334788428c74f167cd73fdad95c3bd8
sameershelar <sameer.shelar@peopleinteractive.in>
2022-05-31 00:29:35 +0530

    -Added logic for validating the Add Custom Drink form. -Added logic for inserting the custom drink into the database.

--- app/src/main/java/com/sameershelar/bloodalcoholcalculator/ui/AddCustomDrinkDialogFragment.kt
+++ app/src/main/java/com/sameershelar/bloodalcoholcalculator/ui/AddCustomDrinkDialogFragment.kt
@@ -1,12 +1,18 @@
 package com.sameershelar.bloodalcoholcalculator.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import com.sameershelar.bloodalcoholcalculator.databinding.FragmentAddCustomDrinkDialogBinding
+import com.sameershelar.bloodalcoholcalculator.vm.AddCustomDrinkViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AddCustomDrinkDialogFragment : DialogFragment() {
 
     companion object {
@@ -14,6 +20,7 @@ class AddCustomDrinkDialogFragment : DialogFragment() {
     }
 
     private lateinit var binding: FragmentAddCustomDrinkDialogBinding
+    private val viewModel: AddCustomDrinkViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +30,9 @@ class AddCustomDrinkDialogFragment : DialogFragment() {
 
         binding.apply {
             addNewDrinkButton.setOnClickListener {
-                /*if (validateFields()) {
+                if (validateFields()) {
                     createNewDrinkAndAddInDb()
-                }*/
+                }
             }
         }
 
@@ -33,10 +40,84 @@ class AddCustomDrinkDialogFragment : DialogFragment() {
     }
 
     private fun createNewDrinkAndAddInDb() {
-        TODO("Not yet implemented")
+        val drinkName = binding.drinkNameEdit.editText?.text.toString()
+        val drinkVolume = binding.volumeEdit.editText?.text.toString().toInt()
+        val abv = binding.abvEdit.editText?.text.toString().toDouble()
+        viewModel.addCustomDrink(drinkName, drinkVolume, abv)
+        dismiss()
     }
 
     private fun validateFields(): Boolean {
-        TODO("Not yet implemented")
+        val drinkName = binding.drinkNameEdit.editText?.text.toString()
+        val drinkVolume = binding.volumeEdit.editText?.text.toString()
+        val abv = binding.abvEdit.editText?.text.toString()
+
+        //Validating empty text
+        if (drinkName.isEmpty()) {
+            Toast.makeText(
+                requireContext(),
+                "Name cannot be empty",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+        if (drinkVolume.isEmpty()) {
+            Toast.makeText(
+                requireContext(),
+                "Volume cannot be empty",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+        if (abv.isEmpty()) {
+            Toast.makeText(
+                requireContext(),
+                "Alcohol content cannot be empty",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+
+        //Validating type and valid range
+        try {
+            val drinkVolumeInt = drinkVolume.toInt()
+            if (drinkVolumeInt < 30 || drinkVolumeInt > 2000) {
+                Toast.makeText(
+                    requireContext(),
+                    "Please enter valid volume",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return false
+            }
+        } catch (e: NumberFormatException) {
+            Log.d(TAG, "Invalid drink volume: ${e.message}")
+            Toast.makeText(
+                requireContext(),
+                "Please enter valid volume",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false;
+        }
+        try {
+            val abvDouble = abv.toDouble()
+            if (abvDouble <= 0 || abvDouble > 100) {
+                Toast.makeText(
+                    requireContext(),
+                    "Please enter valid alcohol content",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return false
+            }
+        } catch (e: NumberFormatException) {
+            Log.d(TAG, "Invalid alcohol content: ${e.message}")
+            Toast.makeText(
+                requireContext(),
+                "Please enter valid alcohol content",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false;
+        }
+
+        return true;
     }
 }
